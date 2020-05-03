19775804548a9d05cc80d8e7d30834000a67bc4f
fsoblet <mail@soblet.be>
2020-05-03 18:08:28 +0200

    optimize table of 7

--- table-7.py
+++ table-7.py
@@ -1,8 +1,6 @@
 def multiplied_by(num):
-    i=0
-    while i < 10:
-        print(i+1, "*", num , "=" , (i+1) * num)
-        i += 1
+    for i in range(num,10*num,num):
+        print(i)
 
-number=input("Enter a number to get his table")
+number=input("Enter a number to get his table : ")
 multiplied_by(int(number))
